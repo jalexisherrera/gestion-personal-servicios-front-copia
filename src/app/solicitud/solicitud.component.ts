aa66ebadccaab3611c409c87864214843d76b132
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-04-03 23:13:10 -0500

    Update solicitud.component.ts

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { SolicitudService } from '../solicitud.service';
 
 @Component({
@@ -9,17 +10,83 @@ import { SolicitudService } from '../solicitud.service';
 export class SolicitudComponent implements OnInit {
 
   solicitud: any[] = [];
+  formulario: FormGroup = this.fb.group({
+    descripcion: [],
+    estado: [false]
+  });
+  solicitudEnEdicion: any;
 
   constructor(
-    private solicitudServices: SolicitudService
+    private solicitudServices: SolicitudService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.getAll();
+    
+    }
+
+  getAll(){
     this.solicitudServices.getAll()
     .subscribe((solicitud: any) => {
       console.log('solicitud', solicitud);
       this.solicitud = solicitud._embedded.solicitudes;
     })
+  }
+
+  save(){
+    const values = this.formulario.value;
+
+    console.log('values', values);
+
+    let request;
+
+    if(this.solicitudEnEdicion){
+      
+      request = this.solicitudServices.update(this.solicitudEnEdicion._links.self.href, values)
+
+    }else{
+      
+      request = this.solicitudServices.create(values);
+
+    }
+
+    request
+      .subscribe({
+        next: () => {
+          this.getAll();
+          this.solicitudEnEdicion = null;
+          this.formulario.setValue({
+            descripcion: '',
+            estado: false
+          })
+        },
+        error: () => {
+
+        }
+        
+      })
+
+    
+  }
+
+  editar(solicitud: any){
+    this.solicitudEnEdicion = solicitud;
+
+    this.formulario.setValue({
+      descripcion: solicitud.descripcion,
+      estado: solicitud.estado
+    })
+  }
+
+  eliminar(solicitud: any){
+    const ok = confirm('¿Estás seguro de eliminar esta tarea?');
+    if(ok){
+      this.solicitudServices.delete(solicitud._links.self.href)
+      .subscribe(() =>{
+        this.getAll();
+      });
+    }
 
   }
 
